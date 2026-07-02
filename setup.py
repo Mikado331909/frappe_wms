d71be80bd0e6239675edfeed25551703fa3bf0fb
Mika Skov <mika.skov@gmail.com>
2026-07-02 12:05:42 +0200

    Simplify WMS package install

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 from frappe_wms import __version__ as version
 
-with open("requirements.txt") as f:
+ROOT = Path(__file__).parent
+
+with (ROOT / "requirements.txt").open() as f:
     install_requires = [
         line.strip()
         for line in f.read().splitlines()
@@ -13,10 +17,13 @@ setup(
     name="frappe_wms",
     version=version,
     description="Lightweight WMS location layer for ERPNext",
+    long_description=(ROOT / "README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
     author="Crings BV",
     author_email="sales@crings.eu",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     install_requires=install_requires,
+    python_requires=">=3.10",
 )
